b8b69f35cbf911d0c795459b765fad15d9734042
Ikenna <ikenna@kali.Ikenna-PC>
2022-10-29 17:19:44 -0400

    a python code that pass pycodestyle

---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a function that adds number
+"""
+
+
+def add(a, b):
+    """
+    add function
+    """
+
+    return a + b
+
+
+print(' the result is {}'.format(add(2, 3)))
