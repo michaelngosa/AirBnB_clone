35d0a3872c7f74612d4e4208d0aa5fe963ea65e1
Ikenna <ikenna@kali.Ikenna-PC>
2022-10-30 23:20:28 -0400

    corrected the console with quit and EOF command

--- console.py
+++ console.py
@@ -9,38 +9,40 @@ class HBNBCommand(cmd.Cmd):
     """
     HBNBCommand class
     """
-    
+
     prompt = '(hbnb)'
+
     def do_quit(self, command):
         """
         to exit the program
         """
         exit()
-    
+
     def help_quit(self):
         """
         help for quit command
         """
         print('Quit command to exit the program\n')
-        
+
     def do_EOF(self, command):
         """
         EOF method
         """
         print()
         exit()
-    
+
     def help_EOF(self):
         """
         help for EOF
         """
         print('EOF command to exit the program\n')
-    
+
     def emptyline(self):
         """
         an empty line + ENTER shouldn’t execute anything
         """
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
