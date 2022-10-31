166cce6a6108fb4cb4c9c3681e2e41641aac6c37
Ikenna <ikenna@kali.Ikenna-PC>
2022-10-31 14:59:34 -0400

    updated the models

--- models/city.py
+++ models/city.py
@@ -5,13 +5,14 @@
 from models.base_model import BaseModel
 
 
-
-
-
 class City(BaseModel):
-
     """temp"""
 
     state_id = ""
-
     name = ""
+
+    def __init__(self, *args, **kwargs):
+            """
+            init
+            """
+            super().__init__(*args, **kwargs)
--- models/state.py
+++ models/state.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-
 """includes State class"""
 
 from models.base_model import BaseModel
 
 
-
-
-
 class State(BaseModel):
-
     """temp"""
 
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        init
+        """
+        super().__init__(*args, **kwargs)
