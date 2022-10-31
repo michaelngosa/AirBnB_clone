0a7340956e17a19082096a2d6e9e9db9560ffb7b
Ikenna <ikenna@kali.Ikenna-PC>
2022-10-30 20:29:04 -0400

    created the basemodel class and the init file

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+from models.engine.file_storage import FilestSorage
+
+storage = FilestSorage()
+storage.reload()
---
+++ models/base_model.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+'''
+Write a class BaseModel that defines all common attributes/methods
+for other classes
+'''
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    BaseModel class
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Constructor method
+        """
+
+        from models import storage
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            self.save()
+            storage.new(self)
+        else:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
+
+    def save(self):
+        """
+        updates the public instance attribute
+        updated_at with the current datetime
+        """
+
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def __str__(self):
+        """
+        string representation of BaseModel class
+        """
+        return '[{}] ({}) {}'.format(
+            type(self).__name__, self.id, self.__dict__)
+
+    def to_dict(self):
+        """
+        dictionary containing all the key/values of __dict__
+        """
+        my_dict = dict(self.__dict__)
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict['__class__'] = type(self).__name__
+        return my_dict
